d88ef9be2f8f2249fbef6bc364a3a081570bebf4
kmezhoud <kmezhoud@gmail.com>
2022-01-25 18:26:56 +0100

    add biodiversity launcher

---
+++ R/biodiversity.R
@@ -0,0 +1,28 @@
+#' Launch biodiversity with default browser
+#' @description Launch biodiversity with default browser
+#' @return  shiny webpage
+#' @usage biodiversity()
+#'
+#' @examples
+#' ShinyApp <-  1
+#' \dontrun{
+#' biodiversity()
+#' }
+#'
+#' @name biodiversity
+#' @docType package
+#' @import shiny leaflet leaflet.extras shinythemes
+#' @import tidyverse data.table
+#' @importFrom utils installed.packages
+#' @rawNamespace import(geojsonio, except= c(validate))
+#' @export
+
+biodiversity <- function(){
+  if("biodiversity" %in% installed.packages()){
+    library(biodiversity)
+    shiny::runApp(system.file("biodiversity", package = "biodiversity"),
+                  launch.browser = TRUE, quiet = TRUE)
+  }else{
+    stop("Install and load biodiversity package before to run it.")
+  }
+}
--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # biodiversity Map Tracker
 
-#Keywords
+# Keywords
 + Freq: How many time the Spacy was found in different place or date.time
 + Total: The sum of all individus
 + User can search by Kingdom class using checkbox panel
 
-#Issues
+# Issues
 + addSearchFeatures highlight multiple circles with same name
 + addSearchFeatures with multiple addCircles groups
 + All Kingdoms must be checked for the `addSearchFeatures`
